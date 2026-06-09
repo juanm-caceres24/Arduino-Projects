db8205980df7f7c64154dd24a19537e61f014936
juanm-caceres24 <juanmanuel.caceres24@outlook.com>
2026-06-09 17:19:24 -0300

    Marlin 2.1.2.7 stable version with dual Y endstop

--- Arduino-MarlinSoftwareCNCRouter/Marlin-2.1.2.7-custom/Marlin-2.1.2.7/Marlin/Configuration.h
+++ Arduino-MarlinSoftwareCNCRouter/Marlin-2.1.2.7-custom/Marlin-2.1.2.7/Marlin/Configuration.h
@@ -1061,7 +1061,7 @@
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
-#define USE_YMIN_PLUG
+//#define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
 //#define USE_IMIN_PLUG
 //#define USE_JMIN_PLUG
@@ -1070,7 +1070,7 @@
 //#define USE_VMIN_PLUG
 //#define USE_WMIN_PLUG
 //#define USE_XMAX_PLUG
-//#define USE_YMAX_PLUG
+#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
 //#define USE_IMAX_PLUG
 //#define USE_JMAX_PLUG
